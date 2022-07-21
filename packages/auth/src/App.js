0bf12aa33fc7a2322a4a3b058c67221106c879f1
mihai.sturza <mihai.sturza@lateral-inc.com>
2022-07-21 16:33:37 +0300

    added auth subapp

---
+++ packages/auth/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Switch, Route, Router } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+import Signin from './components/Signin';
+import Signup from './components/Signup';
+
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'au'
+})
+
+export default ({ history, onSignin }) => {
+    return (
+        <div>
+            <StylesProvider generateClassName={generateClassName}>
+                <Router history={history}>
+                    <Switch>
+                        <Route path="/auth/signin"><Signin onSignin={onSignin} /></Route>
+                        <Route path="/auth/signup/"><Signup onSignin={onSignin} /></Route>
+                    </Switch>
+                </Router>
+            </StylesProvider>
+        </div>
+    )
+}
--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,8 +1,12 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import Header from './components/Header';
-import MarketingApp from './components/MarketingApp';
+import React, { lazy, Suspense, useState } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+import Header from './components/Header';
+import Progress from './components/Progress';
+
+
+const MarketingLazy = lazy(() => import('./components/MarketingApp'))
+const AuthLazy = lazy(() => import('./components/AuthApp'))
 
 const generateClassName = createGenerateClassName({
     productionPrefix: 'co'
@@ -10,11 +14,17 @@ const generateClassName = createGenerateClassName({
 
 
 export default () => {
+    const [isSignin, setIsSignin] = useState(false);
     return (
         <BrowserRouter>
             <StylesProvider generateClassName={generateClassName}>
                 <Header />
-                <MarketingApp />
+                <Suspense fallback={<Progress />}>
+                    <Switch>
+                        <Route path="/auth"><AuthLazy onSignin={() => setIsSignin(true)} /></Route>
+                        <Route path="/" component={MarketingLazy} />
+                    </Switch>
+                </Suspense>
             </StylesProvider>
         </BrowserRouter>
     )
